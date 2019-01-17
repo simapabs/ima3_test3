35652f92647ed561725ad73a495d968f29391b4d
Manu <Manu@10.101.0.222>
2019-01-17 09:15:45 +0100

    initail Version Klasse Student

---
+++ Test3GitÜbung/src/Student.java
@@ -0,0 +1,20 @@
+
+public class Student {
+	
+private String name;
+private Integer gebJahr;
+private String studiengang;
+
+public static void main(String[]args) {
+	
+}
+
+public Student(String name, Integer gebJahr, String studiengang) {
+	super();
+	this.name = name;
+	this.gebJahr = gebJahr;
+	this.studiengang = studiengang;
+}
+
+
+}
